1182382eea803c14f0fd0caafe7cdb9f252273d5
GillainHsi <alexia.shaowei@gmail.com>
2020-09-24 15:59:27 +0800

    modified war

--- src/main/java/dev/spbt/exam/GillainApplication.java
+++ src/main/java/dev/spbt/exam/GillainApplication.java
@@ -2,9 +2,10 @@ package dev.spbt.exam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GillainApplication {
+public class GillainApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GillainApplication.class, args);
